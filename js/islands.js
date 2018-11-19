3773c523fed1a2d98a36d562a1804ac38ac6a9d2
Fabian Krautgasser <fkrautgasser@posteo.org>
2018-11-19 14:03:49 +0100

    asset specs to files, style to css sheet

---
+++ js/islands.js
@@ -0,0 +1,35 @@
+let islands = {
+    "island_forest": {
+        "position": [-1, 1.6, -1],
+        "scale": [0.06, 0.06, 0.06],
+        "rotation": {
+            "x": 0,
+            "y": 3,
+            "z": 0
+        },
+        "direction": 1,
+        "group": new THREE.Group()
+    },
+    "island_main": {
+        "position": [1, 1, 0],
+        "scale": [0.03, 0.03, 0.03],
+        "rotation": {
+            "x": 0,
+            "y": 3,
+            "z": 0
+        },
+        "direction": 1,
+        "group": new THREE.Group()
+    },
+    "island_lake": {
+        "position": [-1, 0.7, 1],
+        "scale": [0.03, 0.03, 0.03],
+        "rotation": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "direction": 1,
+        "group": new THREE.Group()
+    }
+}
---
+++ js/trees.js
@@ -0,0 +1,10 @@
+let trees = {
+    "beech_tree": {
+        "parent": "island_forest",
+        "scale": [0.02, 0.02, 0.02]
+    },
+    "pine_tree": {
+        "parent": "island_forest",
+        "scale": [0.03, 0.065, 0.03]
+    }
+}
